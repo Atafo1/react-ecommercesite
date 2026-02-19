7e298cbb8df73e7941b961ff5437fe0f6754fd29
Atafo1 <atafodavid9@gmail.com>
2026-02-19 12:30:17 -0100

    fetch data from the backend

--- ecommercesite/src/pages/HomePage.jsx
+++ ecommercesite/src/pages/HomePage.jsx
@@ -3,6 +3,17 @@ import "./HomePage.css";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 export function HomePage() {
+    //fetch gest the data from the backend which in that link
+    fetch('http://localhost:3000/api/products')
+    //,then helps us get the data because fetch there is more like an
+    //asynchronous code it does not finish right away
+    //response is the output given from the backend
+    .then((response)=>{
+        //.json gives us the data attached to the response
+           response.json().then((data) =>{
+               console.log(data);
+           })
+    })
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
