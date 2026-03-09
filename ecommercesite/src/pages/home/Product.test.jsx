7058c7e934254e19ad7558005229564c40c7574b
Atafo1 <atafodavid9@gmail.com>
2026-03-09 11:48:47 -0100

    use beforeEach to remove duplication

--- ecommercesite/src/pages/home/Product.test.jsx
+++ ecommercesite/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import {it,expect,describe,vi} from 'vitest';
+import {it,expect,describe,vi, beforeEach} from 'vitest';
 import {render,screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -7,8 +7,10 @@ import axios from 'axios';
 import { Product } from './Product';
 vi.mock('axios');
 describe('Product component',() => {
-  it('displays  the product details correctly',()=>{
-    const product= {
+   let product;
+  let loadCart=vi.fn();
+beforeEach(()=>{
+  product= {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,7 +21,11 @@ describe('Product component',() => {
     priceCents: 1090,
     keywords: ["socks", "sports", "apparel"]
   };
-  const loadCart=vi.fn();
+  loadCart=vi.fn();
+})
+
+  it('displays  the product details correctly',()=>{
+   
   //vi.fn creates a fake function that is not working and this is called a mock
     render(<Product product={product} loadCart={loadCart}/>);
 
@@ -45,18 +51,7 @@ describe('Product component',() => {
   });
 
   it('adds a product to the cart',async ()=>{
-       const product= {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  };
-  const loadCart=vi.fn();
+     
   //vi.fn creates a fake function that is not working and this is called a mock
     render(<Product product={product} loadCart={loadCart}/>);
 //userEvent helps us simulate actions like clicking a button
